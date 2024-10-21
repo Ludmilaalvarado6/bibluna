852dac9b613405875db0eba90b138ecc0aff3562
Hernan Fresco <herny.12@gmail.com>
2024-10-21 18:52:09 -0300

    modelo terminado v1.0

--- mibibluna_app/admin.py
+++ mibibluna_app/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Empleado, Editorial, Autor, Libro, Socio, Prestamo
+
+@admin.register(Empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = 'id','nombre', 'apellido', 'legajo', 'activo'
+    search_fields = 'nombre', 'apellido'
+    list_filter = 'activo',
+
+@admin.register(Editorial)
+class EditorialAdmin(admin.ModelAdmin):
+    list_display = 'id','nombre', 'telefono', 'email'
+    search_fields = 'nombre',
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = 'id','nombre', 'apellido', 'nacionalidad', 'activo'
+    search_fields = 'nombre', 'apellido', 'nacionalidad'
+    list_filter = 'activo',
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = 'id','titulo', 'ISBN', 'ano_publicacion', 'autor', 'activo'
+    search_fields = 'titulo', 'ISBN'
+    list_filter = 'activo', 'autor'
+
+@admin.register(Socio)
+class SocioAdmin(admin.ModelAdmin):
+    list_display = 'id','nombre', 'apellido', 'direccion', 'telefono'
+    search_fields = 'nombre', 'apellido'
+    
+@admin.register(Prestamo)
+class PrestamoAdmin(admin.ModelAdmin):
+    list_display = 'id','fecha_retiro', 'fecha_devolucion', 'socio', 'libro', 'empleado'
+    list_filter = 'socio', 'libro', 'empleado'
--- mibibluna_app/models.py
+++ mibibluna_app/models.py
@@ -1,77 +1,72 @@
-class Empleados(models.Model):
-    nombre = models.CharField(max_length=38,null=False,default=None)
-    apellido = models.CharField(max_length=38,null=False,default=None)
-    fecha_Contratacion = models.CharField(max_length=38,null=True,default=None)
-    Cargo = models.CharField(max_length=38,null=True,default=None)
-    Telefono = models.charField(max_length=38,null=True,default=None)
+from django.db import models
 
-    def __str__(self) str:
-        return f'nombre: {self.nombre} = apellido: {self.apellido} = fecha_Contratacion: {self.fecha_Contratacion} = Cargo: {self.Cargo} = Telefono {self.Telefono}
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=38,null=False)
+    apellido = models.CharField(max_length=38,null=False)
+    legajo= models.IntegerField(verbose_name='Legajo')
+    activo= models.BooleanField(default=True,verbose_name='Activo')
+
+    def __str__(self):
+        return f'{self.apellido}, {self.nombre}'
 
 class Editorial(models.Model):
-    nombre = models.CharField(max_length=38,null=False,default=None)
+    nombre = models.CharField(max_length=38,null=False)
     telefono = models.CharField(max_length=38,null=True,default=None)
-    gmail = models.charField(maxlenth=38,null=True,default=None)
-    
+    email = models.EmailField(max_length=38,null=True,default=None)
+    def __str__(self) -> str:
+        return self.nombre
+    class Meta:
+        verbose_name_plural = "Editoriales"
+        
+class Autor(models.Model):
+    nombre = models.CharField(max_length =38,null=False)
+    apellido = models.CharField (max_length =38,null=False)
+    nacionalidad = models.CharField (max_length =38,null=True)
+    activo = models.BooleanField(default=True)
+    def __str__(self):
+        return f'{self.apellido}, {self.nombre}'
+    class Meta:
+        verbose_name_plural = 'Autores'
 
 class Libro(models.Model):
-    titulo = models.CharField(max_length=38,null=False,default=None)
-    ISBN = models.CharField(max_length,null=False,default=None)
-    ano_Publcacion = models.IntegerField(max_length,null=True,default=None)
+    titulo = models.CharField(max_length=38,null=False)
+    ISBN = models.CharField(max_length=30,null=False)
+    ano_publicacion = models.IntegerField(null=True,verbose_name='Año de Publicación')
+    autor = models.ForeignKey(
+        Autor,  
+        related_name='autores_libros',
+        on_delete=models.CASCADE,
+        verbose_name='Autor',
+    ) 
+    activo = models.BooleanField(default=True, verbose_name='Activo')
 
-    def __str__(self) str:
-        return f'titulo: {self.titulo} = ISBN {self.ISBN} = ano_Publicacion{self.ano_Publicacion}
+    def __str__(self):
+        return self.titulo
 
-class Autores(models.Model)
-      nombre = models.CherFied(max_lenhtg=38,null=False,default=None)
-      apellido = models.CherFied(max_lenhtg=38,null=False,default=None)
-      nacionalidad = models.CherFied(max_lenhtg=38,null=True,default=None)
 
-      def __str__ str:
-    return f'nombre {self.nombre} = apellido {self.apellido} = nacionalidad {self.nacionalidad}
+class Socio(models.Model):
+    nombre = models.CharField (max_length=38,null=False)
+    apellido = models.CharField (max_length =38,null=False)
+    direccion = models.CharField (max_length =38,null=True)
+    telefono = models.CharField(max_length=38,null=True)
 
-    class Prestamos(models.Model)
-    fecha_prestamos = models.CherFied(max_lenhtg=38,null=False,default=None)
-    fecha_devolucion = models.CherFied(max_lenhtg=38,null=False,default=None)
-
-    def __str__ str:
-        return f'fecha_prestamos {self.fecha_prestamos} = fecha_devolucion {self.fecha_devolucion}
-
-class Socios(models.Model)
-    nombre = models.CherFied(max_lenhth=38,null=False,default=None)
-    apellido = model.CherFied(max_lenhtg=38,null=False,default=None)
-    direccion = model.CherFied(max_lenhtg=38,null=True,default=None)
-    telefono = model.CherField(max_length=38,null=True,default=None)
-    fecha_afiliacion = modelCherField(max_lenhth=38,null=False,default=None)
-
-    def __str__ str: 
-        return f'nombre {self.nombre} = apellido {self.apellido} = direccion {self.direccion } = telefono {self.telefono} = fecha_afiliacion {self.fecha_afiliacion}
+    def __str__(self): 
+        return f'{self.apellido}, {self.nombre}'
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
+class Prestamo(models.Model):
+    fecha_retiro = models.DateTimeField(auto_now_add=True)
+    fecha_devolucion = models.DateTimeField(null=True)
+    socio= models.ForeignKey(Socio,
+                             related_name='prestamos',
+                             on_delete=models.PROTECT) 
+    # Protect evita que se elimine un socio si existe un prestamo asiado a él
+    libro= models.ForeignKey(Libro,
+                             related_name='libros',
+                             on_delete=models.PROTECT)
+    # Protect evita que se elimine un Libro si existe un prestamo asiado a él
+    empleado= models.ForeignKey(Empleado,
+                                related_name='prestamos_realizados',
+                                on_delete=models.PROTECT)
+    def __str__(self):
+        return str(self.id) 
